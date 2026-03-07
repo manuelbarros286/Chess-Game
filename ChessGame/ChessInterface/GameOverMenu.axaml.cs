25e44d35ae9bf5d4d34f82658b26182bc33f1eb5
Manuel Baptista Barros <manuelbarros286@gmail.com>
2026-03-07 22:29:51 +0000

    new game over menu design; restart and quit options UI

---
+++ ChessGame/ChessInterface/GameOverMenu.axaml.cs
@@ -0,0 +1,24 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace ChessInterface;
+
+public partial class GameOverMenu : UserControl
+{
+    public GameOverMenu()
+    {
+        InitializeComponent();
+    }
+
+    private void Restart_Click(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Quit_Click(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
